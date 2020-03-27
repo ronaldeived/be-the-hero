f8858b8ef27a93e41dd3abf80104bf0714f773cf
ronaldeived <ronaldeived@hotmail.com>
2020-03-26 22:56:50 -0300

    front end completo e conectado com o DB

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -26,7 +26,6 @@ module.exports = {
 
     async create(request, response){
         const {title, description, value} = request.body;
-        request.headers;
         const ong_id = request.headers.authorization;
 
         const [id] = await connection('incidents').insert({
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import api from '../../services/api';
 import './styles.css';
 
-import logoImg from '../../assets/logo.svg'
+import logoImg from '../../assets/logo.svg';
 
 export default function Register(){
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    const history = useHistory();
+
+    async function handleRegister(e){
+        e.preventDefault();
+
+        const data = {
+            nome,
+            email,
+            whatsapp,
+            city,
+            uf
+        };
+
+        try {
+            const response = await api.post('/ongs', data);
+            
+            alert(`Seu ID de acesso: ${response.data.id}`);
+
+            history.push('/');
+        } catch (err) {
+            alert('Erro no cadastro, tente novamente.');
+        }
+    }
+
     return (
         <div className="register-container">
             <div className="content">
@@ -17,20 +48,35 @@ export default function Register(){
                     <p>Faça seu cadastro, entre na plataforma e
                         ajude pessoas a encontrarem os casos da sua ONG</p>
 
-                    <Link className="back-link" href="/register">
-                        <FiArrowLeft size={16} color="#E02041"/>
-                        Não tenho cadastro
+                    <Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="#e02041"/>
+                        Tenho cadastro
                     </ Link>
                 </section>
 
-                <form>
-                    <input placeholder="Nome da ONG"/>
-                    <input type="email" placeholder="E-mail"/>
-                    <input placeholder="WhatsApp"/>
+                <form onSubmit={handleRegister}>
+                    <input placeholder="Nome da ONG"
+                        value={nome}
+                        onChange={e => setNome(e.target.value)}
+                    />
+                    <input type="email" placeholder="E-mail"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input placeholder="WhatsApp"
+                        value={whatsapp}
+                        onChange={e => setWhatsapp(e.target.value)}
+                    />
 
                     <div className="input-group">
-                        <input placeholder="Cidade"/>
-                        <input placeholder="UF" style={{ width: 80}}/>
+                        <input placeholder="Cidade"
+                            value={city}
+                            onChange={e => setCity(e.target.value)}
+                        />
+                        <input placeholder="UF" style={{ width: 80}}
+                            value={uf}
+                            onChange={e => setUf(e.target.value)}
+                        />
                     </div>
 
                     <button className="button" type="submit">Cadastrar</button>
